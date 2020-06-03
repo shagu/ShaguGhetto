15a39f9955d7950d102eba9d8a89fae31786d5e3
shagu <ericmauser@googlemail.com>
2020-06-03 20:17:55 +0200

    move invite handler into own function

--- ShaguGhetto.lua
+++ ShaguGhetto.lua
@@ -1,6 +1,31 @@
 local name, text
 local player = UnitName("player")
 local f = CreateFrame("Frame", "ShaguGhetto", UIParent)
+f.SendInvite = function(self, name)
+  if not name then return end
+
+  -- invite player
+  InviteUnit(name)
+
+  -- announce name! (max every 5 seconds)
+  if not self.lastcall or GetTime() - self.lastcall > 5 then
+    local channel = event == "CHAT_MSG_CHANNEL" and "CHANNEL" or "SAY"
+    if player == "Ghettomann" then SendChatMessage(player .. "!", channel, nil, GetChannelName("World")) end
+    self.lastcall = GetTime()
+  end
+
+  ShaguGhetto_config = ShaguGhetto_config or {}
+  ShaguGhetto_config[name] = ShaguGhetto_config[name] or 0
+  ShaguGhetto_config[name] = ShaguGhetto_config[name] + 1
+
+  local count = 0
+  for i, data in pairs(ShaguGhetto_config) do
+    count = count + data
+  end
+
+  DEFAULT_CHAT_FRAME:AddMessage(name .. ": |cff33ffcc" .. ShaguGhetto_config[name] .. " / " .. count)
+end
+
 f:RegisterEvent("CHAT_MSG_SAY") -- enable guild ghetto
 f:RegisterEvent("CHAT_MSG_WHISPER") -- enable guild ghetto
 f:RegisterEvent("CHAT_MSG_YELL") -- enable guild ghetto
@@ -25,7 +50,7 @@ f:SetScript("OnEvent", function()
   if event == "PARTY_INVITE_REQUEST" then
     DeclineGroup()
     StaticPopup_Hide("PARTY_INVITE")
-    InviteUnit(arg1)
+    f:SendInvite(arg1)
     return
   end
 
@@ -41,25 +66,6 @@ f:SetScript("OnEvent", function()
 
   -- invite people when ghetto is found
   if text and name and name ~= player and string.find(string.lower(text), "ghetto") then
-    -- invite player
-    InviteUnit(name)
-
-    -- announce name! (max every 5 seconds)
-    if not this.lastcall or GetTime() - this.lastcall > 5 then
-      local channel = event == "CHAT_MSG_CHANNEL" and "CHANNEL" or "SAY"
-      if player == "Ghettomann" then SendChatMessage(player .. "!", channel, nil, GetChannelName("World")) end
-      this.lastcall = GetTime()
-    end
-
-    ShaguGhetto_config = ShaguGhetto_config or {}
-    ShaguGhetto_config[name] = ShaguGhetto_config[name] or 0
-    ShaguGhetto_config[name] = ShaguGhetto_config[name] + 1
-    DEFAULT_CHAT_FRAME:AddMessage(name .. ": |cff33ffcc" .. ShaguGhetto_config[name])
+    f:SendInvite(name)
   end
 end)
-
-function ShaguGhettoStats()
-  for name, count in pairs(ShaguGhetto_config) do
-    DEFAULT_CHAT_FRAME:AddMessage(name .. ": |cff33ffcc" .. count)
-  end
-end

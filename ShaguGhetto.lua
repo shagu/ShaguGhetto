a622436d953f0bd1658cd171ba9792cd6200c39b
shagu <ericmauser@googlemail.com>
2020-06-03 19:47:39 +0200

    throw invites back and optimize false positives

--- ShaguGhetto.lua
+++ ShaguGhetto.lua
@@ -16,27 +16,28 @@ f:SetScript("OnEvent", function()
   if event == "CHAT_MSG_SYSTEM" then
     local _, _, name = string.find(arg1, string.gsub(ERR_ALREADY_IN_GROUP_S, "%%s", "(%%S+)"))
     if name then
-      SendChatMessage("You're already in a group. Please relog and try again.", "WHISPER", nil, name)
+      SendChatMessage("It says: \"You're already in a group\". Please leave or relog (if bugged) and whisper \"ghetto\" when ready.", "WHISPER", nil, name)
     end
     return
   end
 
-  -- decline invitations
+  -- decline invitation and throw invite back
   if event == "PARTY_INVITE_REQUEST" then
     DeclineGroup()
     StaticPopup_Hide("PARTY_INVITE")
-    SendChatMessage("Sorry, I don't accept invites. You either need to use the word \"ghetto\" in SAY, YELL, WORLD or WHISPER.", "WHISPER", nil, arg1)
+    InviteUnit(arg1)
     return
   end
 
-  -- skip on false-positives
-  if text and name and string.find(string.lower(text), string.lower(player)) then return end
-  if text and name and string.find(string.lower(text), string.lower("ghettoman")) then return end
-  if text and name and string.find(string.lower(text), string.lower("ghetto mann")) then return end
-  if text and name and string.find(string.lower(text), string.lower("ghetto man")) then return end
+  -- remove ghettomann false-positives
+  text = string.gsub(string.lower(text), "ghettoman", "")
+  text = string.gsub(string.lower(text), "ghetto man", "")
 
   -- make sure to leave party if theres no more space left
-  if GetNumPartyMembers() > 3 then LeaveParty() end
+  if GetNumPartyMembers() > 3 then
+    SendChatMessage("There are too many people in this group. Cu :)", "PARTY")
+    LeaveParty()
+  end
 
   -- invite people when ghetto is found
   if text and name and name ~= player and string.find(string.lower(text), "ghetto") then
@@ -53,9 +54,7 @@ f:SetScript("OnEvent", function()
     ShaguGhetto_config = ShaguGhetto_config or {}
     ShaguGhetto_config[name] = ShaguGhetto_config[name] or 0
     ShaguGhetto_config[name] = ShaguGhetto_config[name] + 1
-
-    print(name .. ": |cff33ffcc" .. ShaguGhetto_config[name])
-    AddFriend(name)
+    DEFAULT_CHAT_FRAME:AddMessage(name .. ": |cff33ffcc" .. ShaguGhetto_config[name])
   end
 end)
 
